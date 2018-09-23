312c29845d817f401b57f6c2dd63c75300554fea
AmadyaChz <ceci.dresser96@gmail.com>
2018-09-22 22:27:59 -0500

    debugged

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,27 +1,27 @@
-var shows = ["cat", "dog", "elephant", "giraffe", "mouse", "snake", "fish"];
+var topics = ["cats", "Kermit", "Stranger Things", "dance", "Halloween", "Will Ferrell", "bird", "Sailor Moon"];
 
 function makeButtons(){
     $("#buttonsView").empty();
 
-    for (var i = 0; i < shows.length; i++){
+    for (var i = 0; i < topics.length; i++){
         var a = $('<button>');
-        a.addClass('show');
-        a.attr('data-name', shows[i]);
-        a.text(shows[i]);
+        a.addClass('topic');
+        a.attr('data-name', topics[i]);
+        a.text(topics[i]);
         $("#buttonsView").append(a);
     }
 }
 
-$("#addShow").on("click", function(){
-    var show = $("#show-input").val().trim();
-    shows.push(show);
+$("#addTopic").on("click", function(){
+    var topic = $("#topic-input").val().trim();
+    topics.push(topic);
     makeButtons();
     return false;
 })
 
 function displayGifs(){
-    var show = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&limit=10&api_key=dc6zaTOxFJmzC";
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&limit=10&api_key=W7DxnUmuYwCCfO8Vq7Ij3Hy1hyBveVdu";
 
         $.ajax({url: queryURL, method: "GET"}).done(function (response) {
             console.log(response.data);
@@ -29,15 +29,18 @@ function displayGifs(){
             
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $('<div class=gifs>');
-                var showGif = $('<img>');
-                    showGif.attr('src', results[i].images.fixed_height_still.url);
-                    showGif.attr('title', "Rating: " + results[i].rating);
-                    showGif.attr('data-still', results[i].images.fixed_height_still.url);
-                    showGif.attr('data-state', 'still');
-                    showGif.addClass('gif');
-                    showGif.attr('data-animate', results[i].images.fixed_height.url);
+                var p = $("<p>");
+	 			p.text(results[i].rating);
+	 			var p = $("<p>").text("Rating: " + results[i].rating);
+                var topicGif = $('<img>');
+                    topicGif.attr('src', results[i].images.fixed_height_still.url);
+                    topicGif.attr('title', "Rating: " + results[i].rating);
+                    topicGif.attr('data-still', results[i].images.fixed_height_still.url);
+                    topicGif.attr('data-state', 'still');
+                    topicGif.addClass('gif');
+                    topicGif.attr('data-animate', results[i].images.fixed_height.url);
 
-                gifDiv.append(showGif);
+                gifDiv.append(topicGif);
 
                 $("#gifsView").prepend(gifDiv);
             }
@@ -55,6 +58,6 @@ $(document).on('click', '.gif', function() {
         };
 });
 
-$(document).on('click', '.show', displayGifs);
+$(document).on('click', '.topic', displayGifs);
 
 makeButtons();
